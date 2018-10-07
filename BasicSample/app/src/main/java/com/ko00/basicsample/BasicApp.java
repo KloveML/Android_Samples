a371033bbc68808a551acd76f4c5d3a7083423df
ko00 <ko00ml@163.com>
2018-10-07 19:49:40 +0800

    第一次提交

---
+++ BasicSample/app/src/main/java/com/ko00/basicsample/BasicApp.java
@@ -0,0 +1,10 @@
+package com.ko00.basicsample;
+
+import android.app.Application;
+
+public class BasicApp extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
